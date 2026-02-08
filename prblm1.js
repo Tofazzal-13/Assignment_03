8cbf116f4936d8f953b88f33e5e08a9d0fba703f
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-02-08 14:38:56 +0600

    prblm 1

--- prblm1.js
+++ prblm1.js
@@ -1,8 +1,17 @@
 function newPrice(currentPrice, discount) {
-    if(typeof currentPrice !== "number" || typeof discount !== "number"){
-        return "Invalid"
-    }
-    const discountPrice = (currentPrice * discount) / 100;
-    const finalPrice = currentPrice - discountPrice;
-    return finalPrice.toFixed(3);
+   if(  typeof currentPrice !== "number" ||
+        typeof discount !== "number" ||
+        discount < 0 ||
+        discount > 100){
+     return "invalid";
+   }
+
+   else{
+     const discountPrice = (currentPrice * discount) / 100;
+     const finalPrice = currentPrice - discountPrice;
+     return finalPrice.toFixed(3);
+   }
 }
+
+const result = newPrice(1000,101)
+console.log(result);
